2d4048395e880cecec6dfc5e4bafe289e947fb78
TonNomGitHub <ton-email@exemple.com>
2026-08-19 16:04:25 +0100

    Fix target landing page path to accueil_benin_pay

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Sert les fichiers statiques du projet
 app.use(express.static(__dirname));
 
+// Redirige la racine (/) vers la page d'accueil de la banque
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'accueil_benin...', 'code.html'));
+  res.sendFile(path.join(__dirname, 'accueil_benin_pay', 'code.html'));
 });
 
 module.exports = app;
