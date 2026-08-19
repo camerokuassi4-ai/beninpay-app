81ab3a277217dc665ff0b1806c16b68cbe329df1
TonNomGitHub <ton-email@exemple.com>
2026-08-19 16:17:01 +0100

    Add dynamic routing for sub-pages

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Sert les fichiers statiques (CSS, JS, images)
 app.use(express.static(__dirname));
 
+// Route racine (Page d'accueil)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'accueil_beninpay', 'code.html'));
+});
+
+// Route générique pour charger n'importe quelle page HTML (ex: /connexion/code.html)
+app.get('/:folder/code.html', (req, res) => {
+  const { folder } = req.params;
+  res.sendFile(path.join(__dirname, folder, 'code.html'));
 });
 
 module.exports = app;
