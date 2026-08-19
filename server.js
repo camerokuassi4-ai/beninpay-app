4420a256c87763c36bb956fd86b8f3b27c7e9008
TonNomGitHub <ton-email@exemple.com>
2026-08-19 16:00:20 +0100

    Fix landing page path

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Sert les fichiers statiques du projet
 app.use(express.static(__dirname));
 
-// Redirige la racine (/) vers la page de connexion
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'connexion', 'code.html'));
+  res.sendFile(path.join(__dirname, 'accueil_benin...', 'code.html'));
 });
 
 module.exports = app;
